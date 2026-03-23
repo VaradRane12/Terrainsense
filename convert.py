f38e7f49099dc1284a2534fcf378e555563fbbe0
Arshia Joshi <arshiaamoljoshi@gmail.com>
2026-03-23 11:20:12 +0530

    Convert to onnx

--- convert.py
+++ convert.py
@@ -3,7 +3,7 @@ import onnx
 
 # Step 1 — Export
 print("[1/2] Exporting best.pt to ONNX...")
-model = YOLO("best.pt")
+model = YOLO("final.pt")
 model.export(
     format="onnx",
     imgsz=320,
@@ -13,6 +13,6 @@ model.export(
 
 # Step 2 — Verify
 print("[2/2] Verifying ONNX model...")
-onnx_model = onnx.load("best.onnx")
+onnx_model = onnx.load("final.onnx")
 onnx.checker.check_model(onnx_model)
 print("✅ best.onnx is valid and ready!")
